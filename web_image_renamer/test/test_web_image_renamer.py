5a1f034f69cfca9b9219f8a262f8a68e2d2d948b
Magnus Henkel <magnus.henkel@zoho.com>
2017-04-07 00:55:41 +0200

    Documentation

--- web_image_renamer/test/test_web_image_renamer.py
+++ web_image_renamer/test/test_web_image_renamer.py
@@ -1,14 +1,26 @@
-import unittest
+import pytest 
+import os
+
+from _pytest.tmpdir import tmpdir
 from app.web_image_renamer import PathObj
 
 class TestWebImageRenamer(object):
+  
+    @pytest.fixture
+    def path(self, tmpdir):
+        #p = tmpdir.mkdir("country").mkdir("city").mkdir("thumbs").join("IMG_1234.jpg")
+        #return p 
+        pass
 
-    def setUp(self, tmpdir):
-        p = tmpdir.mkdir("country").join("city")
-        p.write("content")
-        self.pobj = PathObj()
-        assert p.read() == "content"
-    
 
-    def test_path_concatenates_correctly_path_as_list(self):
-       pass 
+#    def setup_class(self, path):
+#        #p.write("content")
+#        self.pobj = PathObj(path)
+#        #assert p.read() == "content"
+
+    def test_path_concatenates_correctly_path_as_list(self, tmpdir, path):
+        p = tmpdir.mkdir("country").mkdir("city").mkdir("thumbs").join("IMG_1234.jpg")
+        touch(str(p))
+        assert os.path.exists(p)
+        self.pobj = PathObj(str(p))
+        #self.pobj.full_path = './country/city/thumbs/IMG_1234.jpg' 
